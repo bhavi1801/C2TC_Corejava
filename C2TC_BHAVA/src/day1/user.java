9b4a3b31f2af8691c6b055cf03bf4fbd0b249823
ETHNOTECH <ETHNOTECH@DESKTOP-J534CID>
2025-07-21 04:01:49 -0700

    addition

---
+++ C2TC_BHAVA/src/day1/user.java
@@ -0,0 +1,19 @@
+package day1;
+
+import java.util.Scanner;
+public class user {
+         public static void main(String[] args) {
+        	 int num1;
+        	 int num2;
+        	 
+        	 System.out.println("Enter the value");
+        	 Scanner scanner = new Scanner(System.in);
+        	 num1= scanner.nextInt();
+        	 num2 = scanner.nextInt();
+        	 
+        	 
+        	 
+        	 System.out.println("Sum of two numbers is " +  (num1 + num2)) ;
+         }
+         }
+

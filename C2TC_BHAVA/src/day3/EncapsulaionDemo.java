e39feb8aee4117d0bb345eccbe7a71a69b026599
ETHNOTECH <ETHNOTECH@DESKTOP-J534CID>
2025-07-22 20:52:14 -0700

    Encapsulation

---
+++ C2TC_BHAVA/src/day3/EncapsulaionDemo.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class EncapsulaionDemo {
+	
+
+	
+		public static void main(String[] args) {
+			
+			//object creation 
+			OopsConceptDemo obj = new OopsConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Pooja");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
+
+	}
+
